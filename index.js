bd52cb8d0342eb06372343e640c17529f4466485
dev-rahim <rahimuddin5915@gmail.com>
2023-03-13 16:47:33 +0600

    created api of category by _id and news by id

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 
-const category = require('./data/category.json')
-const news = require('./data/news.json')
+const categories = require('./data/category.json')
+const newses = require('./data/news.json')
 app.get('/', ((req, res) => {
     res.send('News Api running')
 }))
 app.get('/category', ((req, res) => {
-    res.json(category)
+    res.json(categories)
+}))
+
+app.get('/category/:id', ((req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.json(newses)
+    }
+    const selectedCategories = newses.filter(news => news['category_id'] === id)
+    res.json(selectedCategories)
 }))
 
 app.get('/news', ((req, res) => {
-    res.json(news)
+    res.json(newses)
+}))
+
+app.get('/news/:id', ((req, res) => {
+    const id = req.params.id;
+    const selectedId = newses.find(news => news._id === id)
+    res.json(selectedId)
 }))
 
 app.listen(port, () => {

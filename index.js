310e16059d2b0ec5d209a2e5152c55f99387e26e
dev-rahim <rahimuddin5915@gmail.com>
2023-03-13 15:30:47 +0600

    created news api

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ app.use(cors())
 
 
 const category = require('./data/category.json')
+const news = require('./data/news.json')
 app.get('/', ((req, res) => {
     res.send('News Api running')
 }))
 app.get('/category', ((req, res) => {
     res.json(category)
+}))
+
+app.get('/news', ((req, res) => {
+    res.json(news)
 }))
 
 app.listen(port, () => {
